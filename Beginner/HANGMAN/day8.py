4b462862818f5d21b29e30f19f80bf80b2df5516
Handeneilan <handedemirneilan@gmail.com>
2023-02-13 16:20:10 -0500

    final

---
+++ Beginner/HANGMAN/day8.py
@@ -0,0 +1,8 @@
+# Review: 
+# Create a function called greet(). 
+def greet(name):
+# Write 3 print statements inside the function.
+    print(f'Merhaba {name}')
+    print(f'Nasilsin {name}?')
+# Call the greet() function and run your code.
+greet("Hande")
